e589f90d9312b6476d7c587cd4206064cd50673d
Michael Mumma <mummamw@miamioh.edu>
2015-04-28 15:02:56 -0400

    New Project added
    Nathan what up

---
+++ LifeWorks/src/WSSE.java
@@ -0,0 +1,17 @@
+//import java.io*;
+
+public class WSSE {
+
+	double calculateDifference(double bS, double eS){
+		return Math.pow(bS-eS,2);
+	}
+	
+	void calculateScores(double[] bA, double[] eA) {
+		
+	}
+	
+	void genReport(){
+		
+	}
+	
+}
